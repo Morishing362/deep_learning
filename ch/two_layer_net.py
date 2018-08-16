df9592a23a943fa452eac1391e2aa50e400e1e24
Morishing362 <morisita611@gmail.com>
2018-08-16 22:27:19 +0900

    4.5.2実装

--- ch/two_layer_net.py
+++ ch/two_layer_net.py
@@ -1,22 +1,23 @@
 import sys, os
-sys.path.append(os.pardir)
+sys.path.append(os.getcwd())
 from common.functions import *
-from comonn.fradient import numerical_gradient
+from common.gradient import numerical_gradient
 
 class TwoLayerNet:
-    def __init__(self, input_size, hidden_size, out_size, weight_init_std=0.01):
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
         self.params = {}
-        self.params['W1'] = weight_init_std * np.randome.randn(input_size, hidden_size)
+        self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
         self.params['b1'] = np.zeros(hidden_size)
         self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
-        self.params['b2'] = np.zeros(out_size)
+        self.params['b2'] = np.zeros(output_size)
 
     def predict(self, x):
         W1, W2 = self.params['W1'], self.params['W2']
+        b1, b2 = self.params['b1'], self.params['b2']
 
-        a1 = p.dot(x, W1) + b1
+        a1 = np.dot(x, W1) + b1
         z1 = sigmoid(a1)
-        a2 = np.dot(z1, W2)
+        a2 = np.dot(z1, W2) + b2
         y = softmax(a2)
 
         return y
@@ -37,4 +38,11 @@ class TwoLayerNet:
     def numerical_gradient(self, x, t):
         loss_W = lambda W: self.loss(x, t)
 
-        frads = {}
+        grads = {}
+
+        grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
+        grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
+        grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
+        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
+
+        return grads
